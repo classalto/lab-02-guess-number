a5cef6bbb37a44c67b4a95174df2f997167854d5
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-01-12 14:22:52 -0800

    get dom elements app.js, create basic html tags index.html

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // import functions and grab DOM elements
+const submitButton = document.getElementById('click-submit');
+const resetButton = document.getElementById('reset-button');
+const userNumber = document.getElementById('user-guess');
+const attemptsLeft = document.getElementById('attempts');
+const notifyUser = document.getElementById('notify');
 
 // initialize state
+
 
 // set event listeners to update state and DOM
