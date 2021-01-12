06804f14a2aa11e408289470432e9a8878d1950a
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-01-12 14:31:35 -0800

    intialize two variables that track state in app.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const submitButton = document.getElementById('click-submit');
 const resetButton = document.getElementById('reset-button');
 const userNumber = document.getElementById('user-guess');
-const attemptsLeft = document.getElementById('attempts');
-const notifyUser = document.getElementById('notify');
+const attemptsDisplay = document.getElementById('attempts');
+const notifyDisplay = document.getElementById('notify');
 
 // initialize state
+let attemptsLeft = 4;
+let correctNumber = Math.ceil(Math.random() * 20);
 
 
 // set event listeners to update state and DOM
